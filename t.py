b73aca4a343c033f41bcf5756c10f679444377b1
tohatoha633 <xasanovtohirjon89@gmail.com>
2025-10-23 08:29:29 +0300

    t.py

--- t.py
+++ t.py
@@ -6,7 +6,7 @@ from aiogram import Bot, Dispatcher, types,html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import Command
-TOKEN = "8041784280:AAEL6sy3zc2OkLuMaJtuGE_249nJvMerxqk"
+TOKEN = "8041784280:AAEg2XiiwMw6DT43r2KVvDNfW1UC_xpBUEg"
 
 
 dp = Dispatcher()
@@ -46,3 +46,4 @@ async def main():
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
+
